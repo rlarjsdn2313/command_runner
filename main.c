ffcd21a4ce159d40001bd6ced6de8014e98f4148
rlarjsdn2313 <informationdoor04@gmail.com>
2021-03-18 11:29:14 +0900

    FINISH

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+#define LINE_BUF 10240
+#define BIG_BUF 40960
+
+void run_command(char* command, char* buf) {
+	FILE *fp = NULL;
+	char line[LINE_BUF];
+	
+	if ((fp = popen(command, "r")) == NULL){
+		return;
+	}
+		
+	while (fgets(line, LINE_BUF, fp) != NULL) {
+		strcat(buf, line);
+	}
+			
+	pclose(fp);	
+}
 
 int main() {
-  printf("hello world my name is gunwoo.\n");
-
-  return 0;
+	char buf[BIG_BUF];
+	run_command("ls", buf);
+	
+	printf("%s", buf);
+		
+	return 1;
 }
